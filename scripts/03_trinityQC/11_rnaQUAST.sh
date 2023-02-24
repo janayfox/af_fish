23abcdf60c3ae621f915962e2abc166ab0fbad1e
Janay <janayfox95@gmail.com>
2023-02-24 13:07:06 -0500

    Add script for rnaQuast quality check of trinity assembly

---
+++ scripts/03_trinityQC/11_rnaQUAST.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=0-00:10
+#SBATCH --account=def-barrett
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=janay.fox@mail.mcgill.ca
+
+module purge 
+module load StdEnv/2020
+module load gcc/9.3.0
+module load python/3.8.10 
+module load quast/5.0.2
+
+python /cvmfs/soft.computecanada.ca/easybuild/software/2020/avx512/Compiler/gcc9/quast/5.0.2/bin/quast.py -o quast.output -t 8 -l "Trinity_trim.fa" -e --rna-finding BN.Trinity.fasta
